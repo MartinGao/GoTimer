f9c611843e8bc7fef85bd452c1db36ea440201cb
Martin Gao <Marting.Gao@Gmail.com>
2016-02-11 18:00:02 -0800

    Add Date Label and Date Picker

--- Route/history.route.js
+++ Route/history.route.js
@@ -4,13 +4,10 @@ const Log = mongoose.model('Log');
 module.exports = (app) => {
 
   //Frontend Route
-  app.get('/history/index', (req, res) => {
-    res.render('History/index.html');
+  app.get('/history/list', (req, res) => {
+    res.render('History/list.html');
   });
 
-  app.get('/history/events', (req, res) => {
-    res.render('History/index.html');
-  });
 
   app.get('/history/charts', (req, res) => {
     res.render('History/charts.html');
